0f5d76ca9a4a033c42824b137f5108bbf75fcae6
JuanJoseALV <87106655+JuanJoseALV@users.noreply.github.com>
2022-04-01 00:00:10 -0600

    Modelo compras

---
+++ Libreria internacional/Libreria internacional/Modelos/Modelo_Compras.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libreria_internacional.Modelos
+{
+    public class Modelo_Compras
+    {
+       // public string Foto { get; set; }
+        public int ID { get; set; }
+        public string Correo { get; set; }
+        public int Cantidad_libros { get; set; }
+        public DateTime Fecha_compra { get; set; }
+        public DateTime Fecha_llegada_compra { get; set; }
+        public int Monto_con_IVA { get; set; }
+        public int Monto_sin_IVA { get; set; }
+        public int Monto_final { get; set; }
+        public int Monto_Unidad { get; set; }
+    }
+}
