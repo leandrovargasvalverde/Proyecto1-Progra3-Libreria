8b7b75a9a26f5f89d5ba13526b1fad6b4a3a8428
JuanJoseALV <87106655+JuanJoseALV@users.noreply.github.com>
2022-03-31 23:25:47 -0600

    SQL

---
+++ Libreria internacional/Libreria internacional/SQL scrps/Crear tabla Libros.sql
@@ -0,0 +1,21 @@
+USE [Base_Libros]
+GO
+
+/****** Object:  Table [dbo].[Taba_Libros]    Script Date: 30/3/2022 23:06:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Taba_Libros](
+	[ISBN] [int] IDENTITY(1,1) NOT NULL,
+	[Foto] [varchar](200) NULL,
+	[Titulo] [varchar](200) NULL,
+	[Autor] [varchar](200) NULL,
+	[Fecha_de_publicacion] [varchar](50) NULL,
+	[Precio] [money] NULL
+) ON [PRIMARY]
+GO
+
+
