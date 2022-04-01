fab004041a70e0427309cadfbe85637207db87fc
JuanJoseALV <87106655+JuanJoseALV@users.noreply.github.com>
2022-04-01 11:58:42 -0600

    Sql

---
+++ Libreria internacional/Libreria internacional/SQLScripts/Insertar Libros.sql
@@ -0,0 +1,34 @@
+USE [Base_Libros]
+GO
+
+INSERT INTO [dbo].[Tabla_Libros]
+           ([Foto]
+           ,[Titulo]
+           ,[Autor]
+           ,[Fecha_de_publicacion]
+           ,[Precio])
+     VALUES
+           ('\Assets\Imagenes\1.jpg'
+           ,'Señor De Los Anillos 1'
+           ,'J. R. R. TOLKIEN'
+           ,'2012'
+           ,10800)
+GO
+USE [Base_Libros]
+GO
+
+INSERT INTO [dbo].[Tabla_Libros]
+           ([Foto]
+           ,[Titulo]
+           ,[Autor]
+           ,[Fecha_de_publicacion]
+           ,[Precio])
+     VALUES
+           ('\Assets\Imagenes\2.jpg'
+           ,'Harry Potter 1'
+           ,'J. K. ROWLING'
+           ,'2016'
+           ,14800)
+GO
+
+
